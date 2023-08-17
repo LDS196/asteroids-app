d1d3d13b68e48d320e8909a6829185db6245bea3
LDS196 <lihachevd87@gmail.com>
2023-08-18 00:26:53 +0400

    fix: styles

--- app/components/AsteroidPage/AsteroidPage.tsx
+++ app/components/AsteroidPage/AsteroidPage.tsx
@@ -5,6 +5,7 @@ import Button from '@/app/components/Button/Button'
 import ApproachItem from '@/app/components/AsteroidPage/ApproachItem'
 import s from './AsteroidPage.module.scss'
 import Link from 'next/link'
+
 type Props = {
   data: AndroidType
 }
@@ -13,19 +14,21 @@ const AsteroidPage = ({ data }: Props) => {
 
   return (
     <div>
-      <Link href={'/'}><Button title={'назад'}/></Link>
-      <AsteroidItem data={data} />
-      <ul className={s.list}>
-        <h3>Список сближений астеройда:</h3>
+      <Link href={'/'}>
+        <Button title={'назад'} />
+      </Link>
+      <div className={s.asteroid}>
+        <AsteroidItem data={data} />
+        <ul className={s.list}>
+          <h3>Список сближений астеройда:</h3>
 
-        {approach.map((el) => (
-          <li className={s.item} key={el.close_approach_date_full}>
-            <ApproachItem data={el} />
-          </li>
-        ))}
-      </ul>
-
-
+          {approach.map((el) => (
+            <li className={s.item} key={el.close_approach_date_full}>
+              <ApproachItem data={el} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
