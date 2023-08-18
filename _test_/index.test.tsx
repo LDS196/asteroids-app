e0f7b8d9d20a7256f12455ca5323ed4b667623c4
LDS196 <lihachevd87@gmail.com>
2023-08-18 17:48:05 +0400

    feat: add test

---
+++ _test_/index.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react'
+import Title from '@/app/components/Title/Title'
+
+
+describe('Title', () => {
+  it('renders a heading', () => {
+    render(<Title/>)
+  })
+})
