dd8c78e3a3ad10418ef296d69a3e82b76286414a
LDS196 <lihachevd87@gmail.com>
2023-08-21 17:51:20 +0400

    feat: add test

--- _test_/index.test.tsx
+++ _test_/index.test.tsx
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import Title from '@/app/components/Title/Title'
+import { screen } from '@testing-library/dom'
+import Button from '@/app/components/Button/Button'
 
-describe('Title', () => {
-  it('renders a heading', () => {
+
+describe('Main page', () => {
+  it('Should render Title', () => {
     render(<Title />)
+    const header = screen.getByRole('heading')
+    const headerText = 'ARMAGEDDON 2023'
+    expect(header).toHaveTextContent(headerText)
+  })
+  it('Should render Button', () => {
+    render(<Button title={'заказать'}/>)
+    const btnOrder = screen.getByRole('button')
+    expect(btnOrder).toHaveTextContent('заказать')
   })
 })
